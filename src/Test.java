5ca5e78c69eabfee31c3f3fd145bef45e022bcb3
Andrey <andreybabich2012@yandex.ru>
2020-12-22 03:39:29 +0300

    new

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         try {
-            //Customer c1 = new Customer("123456789", "Chuharev Nikita Sergeevich");// Выдаёт ошибку
             Customer c2 = new Customer("123456789102", "Chuhareva Alisa Vasilievna");
             Customer c3 = new Customer("123456789103", "Chuharev Vadim Sergeevich");
             Customer c4 = new Customer("123456789105", "Latisheb Maxim Petrovich");
@@ -10,8 +9,9 @@ public class Test {
             Customer.Check(c3);
             Customer.Check(c4);
             Customer.Check(c5);
+            Customer c1 = new Customer("123456789", "Chuharev Nikita Sergeevich");
         }catch (UnacceptableInnException e){
-            System.out.println("Невверно задан ИНН");
+            System.out.println("Неверно задан ИНН");
         }
 
     }
